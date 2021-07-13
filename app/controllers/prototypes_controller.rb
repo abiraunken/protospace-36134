0a918162a3a84794a1b99b91ff5f3f16255fae06
abiraunken <g.maeshun@gmail.com>
2021-07-13 17:45:37 +0900

    ff

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,8 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index] 
+  before_action :set_prototype, only: [:update,:destroy,:edit,:show]
+  before_action :move_to_index, except: [:new,:create,:index, :show]
+
  def index
   @prototype=Prototype.all
 end
@@ -19,10 +23,14 @@ end
 def show
   @prototype = Prototype.find(params[:id])
   @comment=Comment.new
+  @comments=@prototype.comments.includes(:user)
 end
 
 def edit
   @prototype=Prototype.find(params[:id])
+  unless user_signed_in?
+    redirect_to action: :index
+  end
 end
 
 
@@ -33,11 +41,11 @@ def destroy
 end
 
 def update
-  @prototype=Prototype.find(params[:id])
+  @prototype = Prototype.find(params[:id])
   if @prototype.update(prototype_params)
-  redirect_to root_path
+  redirect_to prototype_path(params[:id])
   else
-    render :show
+    render :edit
 end
 end
 
@@ -52,5 +60,16 @@ end
 private
 def  prototype_params
   params.require(:prototype).permit(:title, :catch_copy, :concept,:image).merge(user_id: current_user.id)
+end
+
+def set_prototype
+  @prototype = Prototype.find(params[:id])
+end
+
+def move_to_index
+  if user_signed_in? && current_user.id != @prototype.user.id
+    # 「もし投稿者とログインしているユーザーが違う場合」
+    redirect_to action: :index
+end
 end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_many :prototypes
          validates :name, presence: true
+         validates :profile, presence: true
+         validates :occupation, presence: true
+         validates :position, presence: true
          has_many :comments 
 end
